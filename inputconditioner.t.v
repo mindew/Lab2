957760428d48a559b91955df74a1a32be5ed9147
mindew <minju.kang@students.olin.edu>
2018-10-08 20:52:19 -0400

    testbench working

--- inputconditioner.t.v
+++ inputconditioner.t.v
@@ -10,35 +10,96 @@ module testConditioner();
     wire rising;
     wire falling;
     
-    inputconditioner dut(.clk(clk),
-    			 .noisysignal(pin),
-			 .conditioned(conditioned),
-			 .positiveedge(rising),
-			 .negativeedge(falling));
 
+    reg begintest;
+    wire endtest;
+    wire dutpassed;
+    
+    inputconditioner dut
+    (
+        .begintest(begintest),
+        .endtest(endtest),
+        .dutpassed(dutpassed),
+        .clk(clk),
+        .noisysignal(pin),
+	    .conditioned(conditioned),
+	    .positiveedge(rising),
+	    .negativeedge(falling)
+    );
+
+    inputcondtestbench tester
+    (
+        .begintest(begintest),
+        .endtest(endtest),
+        .dutpassed(dutpassed),
+        .clk(clk),
+        .noisysignal(pin),
+        .positiveedge(rising),
+        .negativeedge(falling)
+    );
 
     // Generate clock (50MHz)
     initial clk=0;
     always #10 clk=!clk;    // 50MHz Clock
     
-    initial begin
 
-
-        $dumpfile("inputconditioner.vcd");
-        $dumpvars();
-
-        // // SB Indicates should be
-        // // Test case 1, conditioned 1, posedge 1, negedge 0    
-        // $display("Conditioned | Posedge | Negedge | SB Conditioned | SB Posedge | SB Negedge");
-        // clk=clk;pin=1;#1000
-        // $display("%b | %b | %b | 1 | 1 | 0",  conditioned, rising, falling);
-
-
-
-        // Test case 2, conditioned 2, posedge 1, 
-        $finish();
-
-
+    always @(endtest) begin
+        $display("Test passed?: %b", dutpassed);
     end
-    
+
 endmodule
+
+
+module testConditionerTestBench
+(
+input begintest,
+output reg  endtest,
+output reg  dutpassed,
+
+input noisysignal,
+output reg conditioned,
+output reg positiveedge,
+output reg negativeedge,
+output reg clk
+);
+
+    initial begin
+        positiveedge = 0;
+        negativeedge = 0;
+        conditioned = 0;
+        clk = 0;
+    end
+
+
+initial @(begintest) begin
+
+        endtest = 0;
+        dutpassed = 1;
+        #50
+
+        conditioned = 1; positiveedge = 1; negativeedge = 0;
+        if(( conditioned != 1 ) || (positiveedge != 1) || (negativeedge != 0)) begin
+            bitpassed = 0;
+            $display("Test case 1, 1, 0 failed and got %b %b %b", conditioned, positiveedge, negativeedge);
+        end
+        // $dumpfile("inputconditioner.vcd");
+        // $dumpvars();
+
+        // // // SB Indicates should be
+        // // // Test case 1, conditioned 1, posedge 1, negedge 0    
+        // // $display("Conditioned | Posedge | Negedge | SB Conditioned | SB Posedge | SB Negedge");
+        // // clk=clk;pin=1;#1000
+        // // $display("%b | %b | %b | 1 | 1 | 0",  conditioned, rising, falling);
+
+        // // Test case 1:        
+
+        // $finish();
+
+    #5
+    endtest = 1;
+    end
+   
+
+
+
+)
