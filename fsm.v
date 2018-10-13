d70ac30403d592c3056021a1ac1b933e8c7abd95
vickymmcd <vickymmcd@gmail.com>
2018-10-13 17:02:14 -0400

    finished with finite state machine

--- fsm.v
+++ fsm.v
@@ -1,32 +1,41 @@
 `define START  3'd0
 `define GET_BITS  3'd1
-`define READ  3'd2
-`define WRITE  3'd3
-`define END  3'd4
+`define READ1  3'd2
+`define READ2  3'd3
+`define WRITE  3'd4
+`define END  3'd5
 
-module fsm(
+module fsm
+(
   input sclk,
   input cs,
   input shift_register_output,
   output reg miso_bufe,
   output reg dm_we,
   output reg addr_we,
-  output reg sr_we);
+  output reg sr_we
+);
 
   reg [2:0] state;
-  reg [2:0] count <= 3'd0;
+  reg [2:0] count;
 
   always @(posedge sclk) begin
-    if (state == 3'dx)
-      state <= 'START;
+    if (state == 3'dx) begin
+      state <= `START;
+      count <= 3'd0;
+    end
     case(state)
-      'START begin
+      `START: begin
+        miso_bufe = 0;
+        dm_we = 0;
+        addr_we = 0;
+        sr_we = 0;
         if (cs == 0)
-          state <= 'GET_BITS;
+          state <= `GET_BITS;
         else
-          state <= 'START;
+          state <= `START;
       end
-      'GET_BITS begin
+      `GET_BITS: begin
         if (count < 7)
           count <= count + 1;
         else if (count == 7) begin
@@ -34,20 +43,56 @@ module fsm(
             count <= count;
           else if (shift_register_output == 1) begin
             count <= 0;
-            state <= 'READ;
+            state <= `READ1;
           end
           else if (shift_register_output == 0) begin
             count <= 0;
-            state <= 'WRITE;
+            state <= `WRITE;
           end
         end
       end
-      'READ begin
+      `READ1: begin
+        sr_we <= 1;
+        dm_we <= 0;
+        addr_we <= 0;
+        miso_bufe <= 0;
+        state <= `READ2;
       end
-      'WRITE begin
+      `READ2: begin
+        sr_we <= 0;
+        dm_we <= 0;
+        addr_we <= 0;
+        miso_bufe <= 1;
+        if (count < 7)
+          count <= count + 1;
+        else if (count == 7) begin
+          state <= `END;
+          count <= 0;
+        end
       end
-      'END begin
+      `WRITE: begin
+        sr_we <= 0;
+        dm_we <= 1;
+        addr_we <= 0;
+        miso_bufe <= 0;
+        if (count < 7)
+          count <= count + 1;
+        else if (count == 7) begin
+          state <= `END;
+          count <= 0;
+        end
       end
+      `END: begin
+        if (cs == 1)
+          state = `START;
+        sr_we <= 0;
+        dm_we <= 0;
+        addr_we <= 0;
+        miso_bufe <= 0;
+      end
+    endcase
     end
+
+
 
 endmodule
