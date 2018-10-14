5d7a36a6b4fa68675457a25c44546c9663eecc4a
mindew <minju.kang@students.olin.edu>
2018-10-14 01:36:53 -0400

    working on SPI memory.v

--- spimemory.v
+++ spimemory.v
@@ -12,6 +12,36 @@ module spiMemory
     output [3:0]    leds        // LEDs for debugging
 );
 
+// each transaction begins with the chip select cs line being asserted low
+// miso = master in slave out
+// mosi = master out slave in
+// potentially change the format based on the input values
+// should we instantiate those inputs with input conditioner?
+initial begin
+	cs_pin = 0;
+	sclk_pin = 0;
+	mosi_pin = 1;
+	miso_pin = 0; 
+end
+
+	always @(posedge sclk_pin) begin
+		if mosi_pin == 1;
+			shiftregister clkSerialIn(clk,, Finite State Machine, SR_WE, DOUT);
+			// implement address latch and loop it back to address of data memory
+			// SR_WE and DOUT are undefined, yet
+		end
+	end
+
+	always @(negedge sclk_pin) begin
+		dff dffin(clk,sclk_pin,serial_out);
+		// implement not gate from q to miso_pin?
+		// miso_pin != q of dffin
+	end
+
+
+
+
+
 
 endmodule
    
